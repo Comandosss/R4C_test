f19e11b72bfb7b90b7376fe2c19d0dfb5fa9cc61
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2023-10-02 01:47:10 +0300

    added RobotAdmin and registered RobotAdmin, Robot

--- robots/admin.py
+++ robots/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from robots.models import Robot
 
-# Register your models here.
+
+class RobotAdmin(admin.ModelAdmin):
+    list_display = ['serial', 'model', 'version', 'created']
+
+
+admin.site.register(Robot, RobotAdmin)
