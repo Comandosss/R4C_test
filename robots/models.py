a55a2933a94944811b676cb20e19cecf26837135
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2023-10-02 01:49:33 +0300

    added RegexValidator to model and version fields

--- robots/models.py
+++ robots/models.py
@@ -1,8 +1,28 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Robot(models.Model):
     serial = models.CharField(max_length=5, blank=False, null=False)
-    model = models.CharField(max_length=2, blank=False, null=False)
-    version = models.CharField(max_length=2, blank=False, null=False)
+    model = models.CharField(
+        max_length=2,
+        validators=[RegexValidator(
+            r'^[0-9A-Z]{2}$',
+            message='The sequence must contain uppercase characters'
+        )],
+        blank=False,
+        null=False
+    )
+    version = models.CharField(
+        max_length=2,
+        validators=[RegexValidator(
+            r'^[0-9A-Z]{2}$',
+            message='The sequence must contain uppercase characters'
+        )],
+        blank=False,
+        null=False
+    )
     created = models.DateTimeField(blank=False, null=False)
+
+    def __str__(self):
+        return self.serial
