8bce4fe670ef304aed54dccf1edfac432035b85b
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2023-10-02 01:32:03 +0300

    add urlpatterns for R4C

--- R4C/urls.py
+++ R4C/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accounts.views import main_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_view, name='main'),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('orders/', include('orders.urls')),
+    path('customers/', include('customers.urls')),
+    path('robots/', include('robots.urls')),
 ]
